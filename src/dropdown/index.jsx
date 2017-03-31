5350967789a9d8297f9dfba1887c9471ae923a73
zhangwenpei <zhangwenpei@datahunter.cn>
2017-03-31 23:49:46 +0800

    dropdown 组件完善

--- src/dropdown/index.jsx
+++ src/dropdown/index.jsx
@@ -1,76 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Body from './body';
 import Icon from '../icon';
 
 class Dropdown extends React.Component {
-  static defaultProps = {
-
-  }
   static PropTypes = {
       options: React.PropTypes.array.isRequired,
   }
   constructor(props) {
     super(props);
     this.state = {
-      visiable:false,
-      title:props.options[0].value,
+      visiable:true,
+      value:props.options[0].value,
+      firstLoad:true,
     }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleVisible = this.handleVisible.bind(this);
+    this.element = document.createElement('div');
   }
   handleVisible(visiable) {
-    this.setState({visiable})
+    this.renderItem();
+    this.setState({visiable});
   }
-  handelItemClick(key,title) {
-    if(this.props.onClick) {
-      this.props.onClick(key,title);
-      this.handleVisible(false);
-      this.setState({title})
-    }
-  }
-  renderItem(data) {
-    if(Array.isArray(data) && data.length) {
-      this.element = document.createElement('div');
-      
-      this.element.className = 'dh-dropdown-list';
-      document.body.appendChild(this.element);
-      const itemList = (
-        <ul>
-          {
-             data.map(item => (
-               <li
-                 className={item.chek ? 'active' : ''}
-                 key={item.key}
-                 onClick={()=>{this.handelItemClick(item.key,item.value)}}
-                 >
-                   {item.value}
-                 </li>
-             ))
-          }
-        </ul>
-      )
-      ReactDOM.render(itemList,this.element);
-    }
- }
-render() {
+  renderItem() {
     const { options } = this.props;
-    const { title } = this.state;
+    const dropdownEl = this.refs.dropdown;
+    const width = dropdownEl.clientWidth; //dom元素的宽度
+    const height = dropdownEl.clientHeight; //dom元素的高度
+    const top = dropdownEl.getBoundingClientRect().top + height;  //dom元的相对高度 (相对于视窗左上角)
+    const left = dropdownEl.getBoundingClientRect().left; //dom元素的相对宽(相对于视窗左上角)
+    this.element.className = 'dh-dropdown-body';
+    const style = {
+      width,
+      top,
+      left,
+    }
+    document.body.appendChild(this.element);
+    ReactDOM.render((
+      <Body
+        visiable={this.state.visiable}
+        itemDataSource={options}
+        style={style}
+        onClick={this.handleClick}
+        onMouseLeave={this.handleVisible}
+      />
+    ),this.element)
+  }
+  handleClick(key,value) {
+    if(this.props.onClick) {
+      this.props.onClick(key,value);
+      this.setState({
+        value:value,
+        visiable:false,
+      });
+    }
+  }
+ render() {
+    const { value } = this.state;
     return (
       <div
         className="dh-dropdown"
-        onMouseLeave={this.handleVisible.bind(this,false)}
+        ref="dropdown"
         >
         <div
           className="dh-dropdown-title"
-          onClick={this.handleVisible.bind(this,true)}
+          onMouseEnter={() => {this.handleVisible(true)}}
           >
-          <span>{title}</span>
+          <span>{value}</span>
           <Icon type="arrow-down" />
         </div>
-        { options && this.renderItem(options) }
-        {/* <div className={`dh-dropdown-list ${this.state.visiable ? '' : 'dh-dropdown-hidden'}`}>
-           <ul>
-
-           </ul>
-        </div> */}
       </div>
     )
   }
