2c4fbe3f8f61cd13e30423887e9ba6dd6fcb9394
zhangwenpei <venpearzh@gmail.com>
2017-06-14 15:27:59 +0800

    menu modification style

--- stories/menu/menu.jsx
+++ stories/menu/menu.jsx
@@ -18,8 +18,9 @@ class MenuDemo extends React.Component {
     return (
       <div>
         <Menu mode="inline">
-          <Menu.Item>菜单项</Menu.Item>
-          <SubMenu title="inline">
+          <Menu.Item>菜单项1</Menu.Item>
+          <Menu.Item>菜单项2</Menu.Item>
+          <SubMenu title="子菜单inline">
             <Menu.Item key="1">子菜单项1</Menu.Item>
             <Menu.Item key="2">子菜单项1</Menu.Item>
             <Menu.Item key="3">子菜单项1</Menu.Item>
@@ -28,9 +29,31 @@ class MenuDemo extends React.Component {
             <Menu.Item key="6">子菜单项2</Menu.Item>
           </SubMenu>
         </Menu>
-        <Menu mode="vertical">
+        <br/>
+        <hr />
+        <div style={{width: '300px'}}>
+            <Menu>
           <Menu.Item>菜单项</Menu.Item>
-          <SubMenu title="子菜单vertical">
+          <Menu.Item>菜单项</Menu.Item>
+          <Menu.Item>菜单项</Menu.Item>
+          <Menu.Item>菜单项</Menu.Item>
+          <Menu.Item>菜单项</Menu.Item>
+          <SubMenu title="子菜单inline">
+            <Menu.Item key="1">子菜单项1</Menu.Item>
+            <Menu.Item key="2">子菜单项1</Menu.Item>
+            <Menu.Item key="3">子菜单项1</Menu.Item>
+            <Menu.Item key="4">子菜单项1</Menu.Item>
+            <Menu.Item key="5"></Menu.Item>
+            <Menu.Item key="6">子菜单项2</Menu.Item>
+          </SubMenu>
+        </Menu>
+        </div>        
+        <br/>
+        <hr />
+        <div style={{width: '300px'}}>
+        <Menu mode="horizontal">
+          <Menu.Item>菜单项</Menu.Item>
+          <SubMenu title="菜单项子集">
             <Menu.Item key="1">子菜单项1</Menu.Item>
             <Menu.Item key="2">子菜单项1</Menu.Item>
             <Menu.Item key="3">子菜单项1</Menu.Item>
@@ -39,17 +62,7 @@ class MenuDemo extends React.Component {
             <Menu.Item key="6">子菜单项2</Menu.Item>
           </SubMenu>
         </Menu>
-        <Menu mode="inline">
-          <Menu.Item>菜单项</Menu.Item>
-          <SubMenu title="子菜单horizontal">
-            <Menu.Item key="1">子菜单项1</Menu.Item>
-            <Menu.Item key="2">子菜单项1</Menu.Item>
-            <Menu.Item key="3">子菜单项1</Menu.Item>
-            <Menu.Item key="4">子菜单项1</Menu.Item>
-            <Menu.Item key="5">子菜单项1</Menu.Item>
-            <Menu.Item key="6">子菜单项2</Menu.Item>
-          </SubMenu>
-        </Menu>
+        </div>
       </div>
     )
   }
